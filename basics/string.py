f15eeb41a61575043019f7fa7d48a15ed66831af
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-03-08 17:33:50 +0000

    string methods

--- basics/string.py
+++ basics/string.py
@@ -1,10 +1,18 @@
-# course = 'Python for Beginners'
+course = 'Python for Beginners'
 
 # print(course[:])
 
 # formatting
-first = 'John'
-last = 'Doe'
-message = first + ' [' + last + '] is a coder'
-msg = f'{first} [{last}] is a coder'
-print(message)
+# first = 'John'
+# last = 'Doe'
+# message = first + ' [' + last + '] is a coder'
+# msg = f'{first} [{last}] is a coder'
+# print(message)
+
+# string methods
+# print(course.upper())
+# print(course.lower())
+# print(course)
+# print(course.replace('f', 'F'))
+
+print('python' in course)

96781774d3e23966afb2cd70a876d132eb23cf61
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-03-08 16:59:30 +0000

    understanding variables

--- basics/variables.py
+++ basics/variables.py
@@ -10,6 +10,13 @@ is_new=True
 # print(name + "is" + age + "years old and he is a " + is_new + "patien")
 
 
-birth_year = input("Enter your birth year: ")
-age = 2020 - int(birth_year)
-print(age)
+# birth_year = input("Enter your birth year: ")
+# print(type(birth_year))
+# age = 2025 - int(birth_year)
+# print(type(age))
+# print(age)'
+
+weight_lbs = input("Weight in lbs: ")
+weight_kg = int(weight_lbs) * 0.45
+print(type(weight_kg))
+print(weight_kg)

72462213780f6e2f415e4a3a9033f8717bcbf88b
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-03-08 16:55:40 +0000

    understanding variables

---
+++ basics/variables.py
@@ -0,0 +1,15 @@
+name="John"
+age=20
+is_new=True
+
+# name = input("What is your name? ")
+# color = input("What is your favorite color? ")
+
+# print(name + " likes " + color)
+
+# print(name + "is" + age + "years old and he is a " + is_new + "patien")
+
+
+birth_year = input("Enter your birth year: ")
+age = 2020 - int(birth_year)
+print(age)

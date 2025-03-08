fcb860e00be0bc1a4b5921ed10959c8928a4ca3d
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-03-08 18:36:01 +0000

    logical operators

---
+++ basics/logicaloperators.py
@@ -0,0 +1,20 @@
+# AND
+
+has_high_income = False
+has_good_credit = True
+has_criminal_record = True
+
+
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
+
+# OR
+
+# if has_high_income or has_good_credit:
+#     print("Eligible for loan")
+
+# NOT 
+
+if has_good_credit and not has_criminal_record:
+    print("Eligible for loan")
+    

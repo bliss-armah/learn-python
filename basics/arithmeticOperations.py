f7b8775893a5f01486fc70979ab69faf67f82505
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-03-08 18:14:20 +0000

    introduction to math modules

--- basics/arithmeticOperations.py
+++ basics/arithmeticOperations.py
@@ -1,3 +1,5 @@
+import math
+
 # print(10 ** 3)
 
 # # augmented assignment operator
@@ -18,5 +20,13 @@
 # print(x)
 
 # operator precedence
-x = 10 + 3 * 2
-print(x)
+# x = 10 + 3 * 2
+# print(x)
+
+# math functions
+x = 2.9
+print(round(x))
+print(abs(-2.9))
+
+print(math.ceil(2.9))
+print(math.floor(2.9))

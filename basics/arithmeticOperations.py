2f4c9d00d503adc6079270f235c3d12f877d6397
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-03-08 18:07:26 +0000

    arithmetic operations and operator precedence

---
+++ basics/arithmeticOperations.py
@@ -0,0 +1,22 @@
+# print(10 ** 3)
+
+# # augmented assignment operator
+# x = 10
+# x += 3
+# print(x)
+
+# x = 10
+# x -= 3
+# print(x)
+
+# x = 10
+# x *= 3
+# print(x)
+
+# x = 10
+# x //= 3
+# print(x)
+
+# operator precedence
+x = 10 + 3 * 2
+print(x)

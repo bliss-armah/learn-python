2f6697d0b925564d8b65295bce4a30a2f5a2d30b
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-03-09 07:15:04 +0000

    introduction to nested loops

---
+++ basics/nestedloops.py
@@ -0,0 +1,11 @@
+# for x in range(4):
+#   for y in range(3):
+#     print(x,y)
+
+numbers = [1,1,1,1,5]
+
+for n in numbers:
+    output=''
+    for m in range(n):
+        output+= 'x'      
+    print(output)

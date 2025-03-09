e75bd798bcf5f4f2ed82ea5378ae6a4e1622cfbd
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-03-09 17:44:03 +0000

    introduction to functions

---
+++ basics/function.py
@@ -0,0 +1,12 @@
+def greet_user():
+    print('Hi there!')
+    print('Welcome aboard')
+
+
+# function with paramaeters
+
+def greet_user(username):
+    print(f'Hi there {username}')
+    print('Welcome aboard')
+
+greet_user('John')

e086c171db6fc952bcafcbf12bc1754721d80524
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-03-10 00:09:33 +0000

    introduction to functions

--- basics/function.py
+++ basics/function.py
@@ -10,3 +10,17 @@ def greet_user(username):
     print('Welcome aboard')
 
 greet_user('John') 
+
+def emoji_generator(words):
+    splited_words = words.split(' ')
+    emojis = {
+        ":)" : "😊",
+        ":(" : "😞"
+    }
+    output = ''
+    for word in splited_words:
+        output += emojis.get(word,word) + " "
+    return output
+
+message = input("> ")
+print(emoji_generator(message))

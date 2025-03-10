3f153b70c844d6734865071f12dbc0e7cf162da0
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-03-10 23:43:48 +0000

    introduction to packages

--- basics/app.py
+++ basics/app.py
@@ -1,6 +1,9 @@
-import converters
-from converters import kgs_to_lbs
-import utils
+# import converters
+# from converters import kgs_to_lbs
+# import utils
+import ecommerce.shipping
+from ecommerce.shipping import calculate_shipping
 
+# print(utils.find_max([2,5,4,7,1,9]))
 
-print(utils.find_max([2,5,4,7,1,9]))
+calculate_shipping()

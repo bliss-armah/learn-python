228658c0f7dad4889f1ad73aaf3b067861dae5b2
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-03-16 22:25:52 +0000

    understanding path parameters

--- fast-api-tutorials/main.py
+++ fast-api-tutorials/main.py
@@ -2,6 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/blog?limit=10&published=true')
 def index():
-    return 'hey'
+# only get 10 published blogs
+    return {'data':'blog list'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blog'}
+
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'data':id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data':{'1','2'}}

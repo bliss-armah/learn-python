a32c92596adb427ae288aa816086df056b176c9b
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-03-18 23:05:25 +0000

    understanding request body

--- fast-api-tutorials/main.py
+++ fast-api-tutorials/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/blog?limit=10&published=true')
-def index():
+@app.get('/blog')
+def index(limit=10,published:bool=True,sort:Optional[str]=None):
 # only get 10 published blogs
-    return {'data':'blog list'}
+    if published:
+        return {'data':f'{limit} published blogs from the database'}
+    else:
+       return {'data':f'{limit} blogs from the database'}
 
 @app.get('/blog/unpublished')
 def unpublished():
@@ -19,3 +24,14 @@ def show(id:int):
 @app.get('/blog/{id}/comments')
 def comments(id):
     return {'data':{'1','2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    publise: Optional[bool]
+
+
+@app.post('/post')
+def create_blog(request:Blog):
+    return {'data':f'Created blog with titile as {request.title}'}

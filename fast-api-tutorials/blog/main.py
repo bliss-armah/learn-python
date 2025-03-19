c8c4cc6b7934eed881b4db6a61efc585ef4b164f
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-03-19 00:11:43 +0000

    introduction data connection

---
+++ fast-api-tutorials/blog/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from . import schemas,models
+from .database import engine
+
+
+app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
+
+
+@app.post('/blog')
+def create_blog(request:schemas.Blog):
+    return request

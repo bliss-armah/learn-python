6cd7d1c022a1dd4ca3563480e9d2e75ac28123fd
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-04-01 19:14:48 +0100

    implement delete crud functionality

--- fast-api-tutorials/blog/main.py
+++ fast-api-tutorials/blog/main.py
@@ -1,14 +1,49 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status, HTTPException
 from . import schemas,models
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.post('/blog')
-def create_blog(request:schemas.Blog):
-    return request
+
+@app.post('/blog',status_code=status.HTTP_201_CREATED)
+def create_blog(request:schemas.Blog,db:Session=Depends(get_db)):
+   new_blog = models.Blog(title=request.title,body=request.body)
+   db.add(new_blog)
+   db.commit()
+   db.refresh(new_blog)
+   return new_blog
+
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id:int,db:Session=Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f'Blog with id {id} not found')
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+@app.get('/blog')
+def all_blogs(db:Session=Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return  blogs
+
+@app.get('/blog/{id}',status_code=status.HTTP_200_OK)
+def show(id:int,db:Session=Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f'Blog with id {id} not found')
+    return blog

28e0b2b3eb791d0b15fce2b5bdad2663aa571e90
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-04-02 23:26:58 +0100

    introduction to user creation and password hashing

--- fast-api-tutorials/blog/main.py
+++ fast-api-tutorials/blog/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Depends, status, HTTPException
-from . import schemas,models
+from . import schemas,models,hashing
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
 
@@ -35,15 +35,34 @@ def delete_blog(id:int,db:Session=Depends(get_db)):
     db.commit()
     return 'done'
 
-@app.get('/blog')
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id:int,request:schemas.Blog,db:Session=Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f'Blog with id {id} not found')
+    blog.update(request.model_dump())
+    db.commit()
+    return 'updated'
+
+@app.get('/blog',response_model=list[schemas.ShowBlog])
 def all_blogs(db:Session=Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return  blogs
 
-@app.get('/blog/{id}',status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}',status_code=status.HTTP_200_OK,response_model=schemas.ShowBlog)
 def show(id:int,db:Session=Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
         detail=f'Blog with id {id} not found')
     return blog
+
+
+@app.post('/user',status_code=status.HTTP_201_CREATED)
+def create_user(request:schemas.User,db:Session=Depends(get_db)):
+    new_user = models.User(name=request.name,email=request.email,password=hashing.Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user

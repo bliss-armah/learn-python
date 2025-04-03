ed750a2af9ee05d10e3ea37639de790a7f877b27
BlissArmahNwanwah <bliss.extern@emax-digital.com>
2025-04-03 22:52:45 +0100

    docs tags added

--- fast-api-tutorials/blog/main.py
+++ fast-api-tutorials/blog/main.py
@@ -17,7 +17,7 @@ def get_db():
 
 
 
-@app.post('/blog',status_code=status.HTTP_201_CREATED)
+@app.post('/blog',status_code=status.HTTP_201_CREATED,tags=['blogs'])
 def create_blog(request:schemas.Blog,db:Session=Depends(get_db)):
    new_blog = models.Blog(title=request.title,body=request.body)
    db.add(new_blog)
@@ -25,7 +25,7 @@ def create_blog(request:schemas.Blog,db:Session=Depends(get_db)):
    db.refresh(new_blog)
    return new_blog
 
-@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT,tags=['blogs'])
 def delete_blog(id:int,db:Session=Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -35,7 +35,7 @@ def delete_blog(id:int,db:Session=Depends(get_db)):
     db.commit()
     return 'done'
 
-@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED,tags=['blogs'])
 def update_blog(id:int,request:schemas.Blog,db:Session=Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -45,13 +45,13 @@ def update_blog(id:int,request:schemas.Blog,db:Session=Depends(get_db)):
     db.commit()
     return 'updated'
 
-@app.get('/blog',response_model=list[schemas.ShowBlog])
+@app.get('/blog',response_model=list[schemas.ShowBlog],tags=['blogs'])
 def all_blogs(db:Session=Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return  blogs
 
-@app.get('/blog/{id}',status_code=status.HTTP_200_OK,response_model=schemas.ShowBlog)
-def show(id:int,db:Session=Depends(get_db)):
+@app.get('/blog/{id}',status_code=status.HTTP_200_OK,response_model=schemas.ShowBlog,tags=['blogs'])
+def get_blog(id:int,db:Session=Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -59,10 +59,18 @@ def show(id:int,db:Session=Depends(get_db)):
     return blog
 
 
-@app.post('/user',status_code=status.HTTP_201_CREATED)
+@app.post('/user',status_code=status.HTTP_201_CREATED,tags=['users'])
 def create_user(request:schemas.User,db:Session=Depends(get_db)):
     new_user = models.User(name=request.name,email=request.email,password=hashing.Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get('/user/{id}',response_model=schemas.ShowUser,tags=['users'])
+def get_user(id:int,db:Session=Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f'User with id {id} not found')
+    return user
